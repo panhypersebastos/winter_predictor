0faecacefd652f589f5b33bde339004fe956f8a7
David Masson <panhypersebastos@gmail.com>
2018-08-21 20:34:11 +0200

    Progresses with the oo module.

--- winter_predictor.py
+++ winter_predictor.py
@@ -1,5 +1,5 @@
-# This file aims to contain all important code
-# in object-oriented style
+# This file contains all relevant code
+# in object-oriented programming style
 
 import numpy as np
 import logging
@@ -18,37 +18,146 @@ class Predictor:
     ''' This class is about gathering all the predictors as defined by
     Wang et al. (2017). The central output is a DataFrame looking like:
     |WinterYear | PC1_predA_month1 | ... | PC3_predZ_month2|
-    Generally, 'month1' and 'month2' are November and December'''
+    Generally, 'month1' and 'month2' are November and December.
+    The Predictor object will be the same for whatever station
+    to be predicted.'''
 
     def __init__(self):
-        self.con_anom = None,  # ERAINT anomalies collection
-        self.con_grid = None,  # ERAINT grid collection
+        cons = Predictor._initializeCon()
+        self.con_anom = cons['con_anom'],  # ERAINT anomalies collection
+        self.con_grid = cons['con_grid'],  # ERAINT grid collection
         self.anom_df = pd.DataFrame()  # anomalies for a set of grid cells
         self.X_df = pd.DataFrame()  # final DataFrame of predictors
 
-    def initializeCon(self, ERA_vers='lores'):
-        ''' Create the connections to the MongoDB collections '''
-        pass
-
     @staticmethod
-    def _queryAnom(variable, grid_df):
+    def _initializeCon(ERA_vers='lores'):
+        ''' Create the connections to the MongoDB collections '''
+        if (ERA_vers == 'hires'):
+            col_dat = 'ERAINT_monthly'
+            col_anom = 'ERAINT_monthly_anom'
+            col_grid = 'ERAINT_grid'
+            resolution = 0.25
+        elif (ERA_vers == 'lores'):
+            col_dat = 'ERAINT_lores_monthly'
+            col_anom = 'ERAINT_lores_monthly_anom'
+            col_grid = 'ERAINT_lores_grid'
+            resolution = 2.5
+        mongo_host_local = 'mongodb://localhost:27017/'
+        mg = pymongo.MongoClient(mongo_host_local)
+        db = mg.ECMWF
+        return({'con_grid': db[col_grid], 'con_anom': db[col_anom]})
+
+    def _queryAnom(self, variable, grid_df):
         ''' Query anomalies for a given variable and for
         a set of grid cells '''
-        # self.anom_df = anom_df
-        pass
+
+        con_anom = self.con_anom
+        grid_ids = grid_df.id_grid.values
+        res = con_anom.aggregate(pipeline=[
+            {"$project": {"id_grid": 1,
+                          "date": 1,
+                          variable: 1,
+                          "month": {"$month": "$date"}}},
+            {"$match": {"month": {"$in": [9, 10, 11, 12, 1, 2]},
+                        "id_grid": {"$in": grid_ids.tolist()}}},
+            {"$project": {"_id": 0, "id_grid": 1, "date": 1, variable: 1}}
+        ])
+        anom_df = pd.DataFrame(list(res))
+        return anom_df
 
     def _getPCAscores(self, variable, grid_df):
-        ''' blabla '''
-        # Former queryScores function
+        ''' 
+        Get the PCA scores for a given variable and for
+        a set of grid cells.
+        This is the former "queryScores" function.
+        '''
         anom_df = Predictor._queryAnom(variable, grid_df)
-        pass
+        X_df = anom_df.pivot(index='date',
+                             columns='id_grid',
+                             values=variable)
+        pca = PCA(n_components=3)
+        df_scores = pd.DataFrame(pca.fit_transform(X_df),
+                                 columns=['PC1_%s' % (variable),
+                                          'PC2_%s' % (variable),
+                                          'PC3_%s' % (variable)],
+                                 index=X_df.index)
+        return df_scores
 
+    @staticmethod
+    def _genCircle(start_lon, stop_lon, lat, decreasing):
+        ''' Helper function for the _createQueryAboveLat function.
+        In order to create a straight line on a sphere, it is necessary to 
+        generate many points along a given latitude and to connect them.'''
+        res = map(lambda x:[int(x), lat],
+                  sorted(np.arange(start=start_lon, stop=stop_lon+1),
+                         reverse=decreasing))
+        return list(res)
+
+
+    
+    @staticmethod
+    def _createQueryAboveLat(aboveLat):
+        ''' Create geoQuery for the _getGridIds function.'''
+        this_box = {'lonmin': -180,
+                    'lonmax': 180,
+                    'latmin': aboveLat,
+                    'latmax': 90}
+        circle_north_pos = Predictor._genCircle(start_lon=this_box['lonmin'],
+                                                stop_lon=this_box['lonmax'],
+                                                lat=this_box['latmax'],
+                                                decreasing=False)
+        circle_south_neg = Predictor._genCircle(start_lon=this_box['lonmin'],
+                                                stop_lon=this_box['lonmax'],
+                                                lat=this_box['latmin'],
+                                                decreasing=True)
+        slp_poly = [[this_box['lonmin'], this_box['latmin']]]
+        slp_poly.extend(circle_north_pos)
+        slp_poly.extend(circle_south_neg)
+        this_polygon = slp_poly
+
+        if aboveLat > 0:
+            geo_qry = {"loc":
+                       {"$geoWithin": {
+                        "$geometry": {
+                        "type": "Polygon",
+                        "coordinates": [this_polygon]
+                        }}}}
+        else:  # case of a big polygon larger than one hemisphere
+            geo_qry = {"loc":
+                   {"$geoWithin": {
+                       "$geometry": {
+                           "type": "Polygon",
+                           "coordinates": [list(reversed(this_polygon))],  # the orientation matters
+                           "crs": {
+                               "type": "name",
+                               "properties": {"name": "urn:x-mongodb:crs:strictwinding:EPSG:4326" }
+                           }
+                       }
+                   }}}
+        return(geo_qry)
+    
     def _getGridIds(self, aboveLat=None, polygon=None):
         ''' Get the set of all ERAINT grid cells either
         (i) above a given latitude or (ii) within a given polygon '''
-        # This funtion replaces: queryGrids(above) and getGridIds(polygon) 
+        # This funtion replaces: queryGrids(above) and getGridIds(polygon)
         con_grid = self.con_grid
-        pass
+
+        if (aboveLat is not None):
+            geo_qry = Predictor._createQueryAboveLat(aboveLat=aboveLat)
+        elif (polygon is not None):
+            geo_qry = {"loc":
+                       {"$geoWithin": {
+                           "$geometry": {
+                               "type": "Polygon",
+                               "coordinates": polygon
+                   }
+               }}}
+            
+        res = con_grid.find(filter=geo_qry, projection={"_id": 0,
+                                                        "id_grid": 1,
+                                                        "loc": 1})
+        grid_df = pd.DataFrame(list(res))
+        return grid_df
 
     @staticmethod
     def _setWinterYear(date):
@@ -70,17 +179,54 @@ class Predictor:
             month=list(map(lambda x: x.month, df.date)))
         return res_df
 
+    @staticmethod
+    def _renCol(x, mon):
+        '''
+        Create the Predictor DataFrame
+        '''
+        if ('PC' in x or 'Nino' in x):
+            z = '%s_%s' % (x, mon)
+        else:
+            z = x
+        return z
     
+    @staticmethod
+    def createMondf(this_mon, scores_df):
+        mon_df = scores_df.query('month == @this_mon')
+        mon_df.columns = list(
+            map(lambda x: Predictor._renCol(x, mon=this_mon),
+                list(mon_df)))
+        mon_df = mon_df.drop(['date', 'year', 'month'], axis=1)
+        return mon_df
+
     def getPredictorsDF(self):
         ''' This is the main function '''
-        
-        # Get grid cell ids above 20°N and 20°S 
+
+        # Get grid cell ids above 20°N and 20°S
         grid_df_20N = Predictor._getGridIds(aboveLat=20)
         grid_df_20S = Predictor._getGridIds(aboveLat=-20)
 
-        # Get grid cell ids within the Northern Atlantic region
-        grid_df_NAtlantic = Predictor._getGridIds(polygon=.....)
+        # Get grid cell ids within the Northern Atlantic region.
+        poly_NAtlantic = [list(reversed(
+            [[-100, 0], [-100, 45], [-100, 89], [-40, 89],
+             [20, 89], [20, 45], [20, 0], [-40, 0], [-100, 0]]))]
+        grid_df_NAtlantic = Predictor._getGridIds(polygon=poly_NAtlantic)
 
+        # Work with the Nino Index:
+        # No need to compute PCA as Nino index is already
+        # a sythetic value. Take it "as is".
+        # Region to retrieve Niño 3.4 index for SST
+        # Niño 3.4 region: stretches from the 120th to 170th meridians
+        # west longitude astride 
+        # the equator five degrees of latitude on either side (Wikipedia)
+        poly_Nino = [list(reversed([ [-170,-5], [-170,5],[-120,5],
+                                     [-120,-5], [-170,-5]]))]
+        grid_df_Nino = Predictor._getGridIds(polygon=poly_Nino)
+        anom_sst_df = Predictor._queryAnom(variable='sst',
+                                           grid_df=grid_df_Nino)
+        nino_df0 = anom_sst_df[['date', 'sst']].groupby('date').mean().\
+                   reset_index().rename(columns={'sst': 'Nino'})
+        
         # Get the PCA scores
         scores_z70 = Predictor._getPCAscores(variable='z70',
                                              grid_df=grid_df_20N)
@@ -94,6 +240,7 @@ class Predictor:
             columns={"PC1_sst": "PC1_sstna",
                      "PC2_sst": "PC2_sstna",
                      "PC3_sst": "PC3_sstna"})
+        
 
         # Group all predictors in one DataFrame
         scores_df = pd.merge(left=scores_z70,
@@ -109,19 +256,20 @@ class Predictor:
                                              left_index=True,
                                              right_index=True))
         scores_df.reset_index(level=0, inplace=True)
-        scores_df0 = assignWyear(df=scores_df)
-        nino_df = assignWyear(df=nino_df0)
+        scores_df0 = Predictor._assignWyear(df=scores_df)
+        nino_df = Predictor._assignWyear(df=nino_df0)
         scores_df = pd.merge(scores_df0, nino_df)
 
         # Create the Predictor DataFrame based on the months of
         # November and December:
-        sep_df = createMondf(this_mon=9, scores_df=scores_df)
-        oct_df = createMondf(this_mon=10, scores_df=scores_df)
+        sep_df = Predictor._createMondf(this_mon=9, scores_df=scores_df)
+        oct_df = Predictor._createMondf(this_mon=10, scores_df=scores_df)
         X_df = pd.merge(sep_df, oct_df)
 
         # Before applying the Lasso, it is necessary
         # to standardize the predictor ("scander" stuff here)
         # (...)
+        # HERE !!!!!!!!!!!!!!!!
         self.X_df = X_df
 
 
